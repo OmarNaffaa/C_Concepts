d845648ed2d1fb1b6f1415d4e5b5d7127b4e1cff
Omar Naffaa <omarnaffaa.on@gmail.com>
2022-12-30 17:24:12 -0800

    Added basic folder for socket programming demo. Currently client is dummy to practice launching multiple programs in VS code

---
+++ SocketProgramming/SocketClient/socket_client.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 1;
+    printf("Integer: %d\n", i);
+    printf("Hello from client\n");
+}
